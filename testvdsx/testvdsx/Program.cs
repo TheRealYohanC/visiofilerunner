104efd98518f7e9c5af6a11337dd81d119e0e7ed
TheRealYohanC <44784478+TheRealYohanC@users.noreply.github.com>
2019-09-27 12:16:42 -0500

    Simple program to create vsdx file

---
+++ testvdsx/testvdsx/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string startPath = @"sampleflow";
+        string zipPath = @"sampleflow.zip";
+
+        ZipFile.CreateFromDirectory(startPath, zipPath, CompressionLevel.Fastest, false);
+
+        File.Move("sampleflow.zip", Path.ChangeExtension("sampleflow.zip", ".vsdx"));   
+
+        var p = new Process();
+        p.StartInfo = new ProcessStartInfo(@"C:\Users\YOHAN-DESKTOP\source\repos\testvdsx\testvdsx\bin\Debug\netcoreapp3.0\sampleflow.vsdx")
+        {
+            UseShellExecute = true
+        };
+        p.Start();
+
+    }
+}
